3c66ace0befbbd9c7d1eec969a0d951895915583
Gleb Mirolyubov <g.mirolyubov@se15.qmul.ac.uk>
2019-04-16 14:58:13 +0400

    Added player controls with boundaries

---
+++ Arkanoid/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float playerSpeed = 10.0f;
+
+    Vector3 leftBorder;
+    Vector3 rightBorder;
+
+    void Start()
+    {
+        leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5f, 0));
+        rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, 0));
+    }
+
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+
+        transform.Translate(movement * Vector2.right);
+
+        if (transform.position.x < leftBorder.x) {
+            transform.position = new Vector2 (leftBorder.x, transform.position.y);
+        } else if (transform.position.x > rightBorder.x)
+        {
+            transform.position = new Vector2(rightBorder.x, transform.position.y);
+        }
+
+        //transform.position = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 0f);
+    }
+}
