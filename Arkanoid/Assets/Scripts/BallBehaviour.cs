f8403cda792ca18408a92a637f7caa4d5c52a4b9
Gleb Mirolyubov <g.mirolyubov@se15.qmul.ac.uk>
2019-04-16 17:49:34 +0400

    Added walls and ball behaviour

---
+++ Arkanoid/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+
+    private bool ballIsActive;
+    private Vector3 ballPosition;
+    private Vector2 ballInitialForce;
+
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+        // create the force
+        ballInitialForce = new Vector2(100.0f, 300.0f);
+
+        // set to inactive
+        ballIsActive = false;
+
+        // ball position
+        ballPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // check for user input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // check if is the first play
+            if (!ballIsActive)
+            {
+                // add a force
+                rb2D.AddForce(ballInitialForce);
+                // set ball active
+                ballIsActive = !ballIsActive;
+            }
+        }
+    }
+}
---
+++ Arkanoid/Assets/Scripts/WallBoundariesBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBoundariesBehaviour : MonoBehaviour
+{
+
+    public GameObject topWall;
+    public GameObject leftWall;
+    public GameObject rightWall;
+
+    Vector2 leftBorder;
+    Vector2 rightBorder;
+    Vector2 topBorder;
+
+    void Start()
+    {
+        topBorder = Camera.main.ViewportToWorldPoint(new Vector2(0.5f, 1f));
+        leftBorder = Camera.main.ViewportToWorldPoint(new Vector2(0, 0.5f));
+        rightBorder = Camera.main.ViewportToWorldPoint(new Vector2(1, 0.5f));
+
+        AdjustWallsWithScreenSize();
+    }
+
+    void AdjustWallsWithScreenSize ()
+    {
+        topWall.transform.position = topBorder;
+        leftWall.transform.position = leftBorder;
+        rightWall.transform.position = rightBorder;
+    }
+}
