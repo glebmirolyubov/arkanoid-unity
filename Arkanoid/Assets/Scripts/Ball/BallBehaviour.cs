8b7afbf07f55332003f5794d9a8a06a3ef142dfd
Gleb Mirolyubov <g.mirolyubov@se15.qmul.ac.uk>
2019-04-21 19:17:35 +0100

    Final adjustments

--- Arkanoid/Assets/Scripts/Ball/BallBehaviour.cs
+++ Arkanoid/Assets/Scripts/Ball/BallBehaviour.cs
@@ -2,18 +2,15 @@
 
 public class BallBehaviour : MonoBehaviour
 {
-    private Vector2 ballInitialForce;
-
     Rigidbody2D rb2D;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        ballInitialForce = new Vector2(120.0f, 320.0f);
     }
 
     public void LaunchBall()
     {
-        rb2D.AddForce(ballInitialForce);
+        rb2D.velocity = new Vector2(2, 7);
     }
 }
